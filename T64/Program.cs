064671f03e6dabcf76932ad830160455a737f4bf
Anton Stepin <ooosg@rambler.ru>
2022-06-08 16:24:20 +0300

    T64

---
+++ T64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 5; N = 1. -> "5, 4, 3, 2, 1"
+// M = 8; N = 4. -> "8, 7,6, 5, 4"
+
+void NumberFromTo(int FromM, int ToN)
+{
+    if (FromM < ToN)
+    {
+        int temp = ToN;
+        ToN = FromM;
+        FromM = temp;
+    }
+    if (FromM > ToN)
+    {
+        Console.Write($"{FromM}, ");
+        NumberFromTo(FromM - 1, ToN);
+    }
+    else
+    {
+        Console.Write($"{ToN}");
+    }
+}
+
+int From = 1;
+int To = 5;
+NumberFromTo(From, To);
