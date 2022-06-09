8b9aa17e5f8ad1452a0be7e63a7d10ce89328d01
Anton Stepin <ooosg@rambler.ru>
2022-06-09 12:54:36 +0300

    T68

---
+++ T68A/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        n++;
+        return n;
+    }
+    else if (m > 0 && n == 0)
+    {
+       return Akkerman(m - 1, 1);
+    }
+    else
+    {
+       
+         return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
+
+int result = Akkerman(3, 4);
+Console.WriteLine(result);
